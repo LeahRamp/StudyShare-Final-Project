7e32ed96bc0178a33bebec215c9e0b0465ccd17a
LeahRamp <161538918+LeahRamp@users.noreply.github.com>
2025-11-19 22:16:45 +1300

    Added the New post page and created navigation between home page and add post page

--- frontend/src/app/screens/Home.js
+++ frontend/src/app/screens/Home.js
@@ -1,12 +1,14 @@
-import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator, TouchableOpacity } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import PostCard from '../components/PostCard';
 import { getPostsApi } from '../../services/api/posts';
+import { useNavigation } from '@react-navigation/native';
 
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const navigation = useNavigation();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -39,6 +41,13 @@ const Home = () => {
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => <PostCard post={item} />}
       />
+
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => navigation.navigate("AddPost")}
+      >
+        <Text style={styles.addButtonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -52,6 +61,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 30,
+    right: 20,
+    backgroundColor: '#12CE90',
+    width: 65,
+    height: 65,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    shadowOffset: { width: 0, height: 3 },
+  },
+  addButtonText: {
+    color: 'white',
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginBottom: 3,
   },
 });
 
